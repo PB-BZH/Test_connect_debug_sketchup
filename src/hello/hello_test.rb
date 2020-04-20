d9d2ae01292563eaf579bbb8dc05e571e9584cfe
PB-BZH Concept <admin@pb-bzh-concept.fr>
2020-04-20 16:28:28 +0200

    Initial commit

---
+++ src/hello/hello_test.rb
@@ -0,0 +1,9 @@
+#-------------------------------------------------------------------------------------------------------------
+#  Module : Test_connect_debug_sketchup                                                                      -
+#  File name : hello_test.rb - Last modified : 20/04/2020 15:10                                              -
+#                                                                                                            -
+#  PB-BZH Concept (c) 2020 - The software written by Patrick Bourges                                         -
+#  All rights reserved.                                                                                      -
+#  Published 20/4/2020                                                                                       -
+#-------------------------------------------------------------------------------------------------------------
+
